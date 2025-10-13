5b3282189f9e0a806358345fd1d5e626636a877f
Ismail H <greyscale021@gmail.com>
2025-10-13 20:55:19 +0600

    Delete chineseseChef.py

--- chineseseChef.py
+++
@@ -1,6 +0,0 @@
-from Chef import Chef
-class ChineseseChef(Chef):
-    def make_special_dish(self):
-        print("The chef makes an octopass salad")
-    def make_fried_rice(self):
-        print("The chef makes a fried rice")
