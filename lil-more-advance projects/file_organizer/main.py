1f0e5e0e332e2d7399226aff97667c72b7971039
Ismail H <greyscale021@gmail.com>
2025-10-26 14:11:42 +0600

    Added some bug fixes
    Added new other folder and patched some .extenstions

--- lil-more-advance projects/file_organizer/main.py
+++ lil-more-advance projects/file_organizer/main.py
@@ -7,18 +7,21 @@ import os
 
 file_types = {
     'Images': ['.jpg', '.jpeg', '.png', '.gif'],
-    'Documents': ['.pdf', '.docx', '.txt'],
+    'Documents': ['.pdf', '.docx', '.txt','.epub'],
     'Videos': ['.mp4', '.mov', '.avi'],
     'Audio': ['.mp3', '.wav'],
     'Code': ['.py', '.js', '.html', '.css'],
-    'Archives': ['.zip', '.rar']
+    'Archives': ['.zip', '.rar'],
+    'Applications': ['.app', '.appl', '.exe']
 }
 
 folder_path = input("Enter the folder path to organize: ")
+count = 0
 
 for path, subfolder, files in os.walk(folder_path):
     for file in files:
         file_ext = os.path.splitext(file)[1].lower()
+        moved = False
         for category, extensions in file_types.items():
             if file_ext in extensions:
                 category_folder = os.path.join(folder_path, category)
@@ -27,4 +30,16 @@ for path, subfolder, files in os.walk(folder_path):
                 source = os.path.join(path, file)
                 destination = os.path.join(category_folder, file)
                 os.rename(source, destination)
-                print(f"Moved: {file} → {category}")
+                print(f"Moved: {file} -> {category}")
+                count+=1
+                moved = True
+                break
+        if not moved:
+            other_folder = os.path.join(folder_path, "Others")
+            os.makedirs(other_folder, exist_ok=True)
+            source = os.path.join(path, file)
+            os.rename(source, os.path.join(other_folder, file))
+            print(f"Moved: {file} -> Others")
+            count += 1
+
+print(f"Moved {count} files")
